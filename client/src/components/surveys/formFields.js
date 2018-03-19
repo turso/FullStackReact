fc1e7989c79c3b58d38629b9afc053c5a17b250d
Oliver Rikkilä <oliver.rikkila@siilim160906.local>
2018-03-19 13:32:39 +0200

    survey form almost ready

---
+++ client/src/components/surveys/formFields.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    label: 'Survey Title',
+    name: 'title',
+    noValueError: 'You must provide a title'
+  },
+  {
+    label: 'Subject Line',
+    name: 'subject',
+    noValueError: 'You must provide a subject'
+  },
+  {
+    label: 'Email Body',
+    name: 'body',
+    noValueError: 'You must provide a body for the message'
+  },
+  { label: 'Recipient List', name: 'emails' }
+];
